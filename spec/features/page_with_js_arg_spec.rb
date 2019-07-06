182b9a5e0c168f8e6f0b4be28f51d6c89e56d6be
hayashiki <peperoncino.pop@gmail.com>
2019-07-06 21:26:51 +0900

    Apply react

--- spec/features/page_with_js_arg_spec.rb
+++ spec/features/page_with_js_arg_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe 'home page', js: true do
   it 'renders the react component' do
     visit '/'
     expect(page).to have_content("Top#home")
+    expect(page).to have_content("Hello React!")
   end
 end
